ead893c73f439118bca166b14efcfdcc50728899
to4to <justjustifying@outlook.com>
2023-11-15 02:55:36 +0530

    updating customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,14 +1,56 @@
-import React,{useState,useEffect}from 'react'
-import {AnimatePresence,otion} from 'framer-motion'
-import {useSnaphot} from 'valtio'
+
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
-  return (
-    <div>
-      
-      
 
-    </div>
+  const snap = useSnapshot(state)
+  return (
+    <AnimatePresence>
+
+      {
+        !snap.intro && (
+          <>
+            <motion.div
+              key="custom"
+              className="absolute top-0 left-0 z-0 "{...slideAnimation('left')}
+            >
+
+<div className='flex items-centre min-h-screen'>
+  <div   className='editortabs-container tabs '>
+    {
+      EditorTabs.map((tab)=>{
+        <Tab
+        key={tab.name}
+        tab={tab}
+        handleclick={()=>{}}
+        />
+      })
+    }
+
+  </div>
+
+   
+
+</div>
+
+
+
+            </motion.div>
+          </>
+        )
+      }
+
+    </AnimatePresence>
   )
 }
 
