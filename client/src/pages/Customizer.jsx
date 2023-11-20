e7c3d59f62b25abd2722efc729d5ee8843b02924
to4to <justjustifying@outlook.com>
2023-11-21 01:51:19 +0530

    tabs created in customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -25,27 +25,63 @@ const Customizer = () => {
               className="absolute top-0 left-0 z-0 "{...slideAnimation('left')}
             >
 
-<div className='flex items-centre min-h-screen'>
-  <div   className='editortabs-container tabs '>
-    {
-      EditorTabs.map((tab)=>{
-        <Tab
-        key={tab.name}
-        tab={tab}
-        handleclick={()=>{}}
-        />
-      })
-    }
+              <div className='flex items-centre min-h-screen'>
+                <div className='editortabs-container tabs '>
+                  {
+                    EditorTabs.map((tab) => {
+                      <Tab
+                        key={tab.name}
+                        tab={tab}
+                        handleclick={() => { }}
+                      />
+                    })
+                  }
 
-  </div>
-
-   
-
-</div>
+                </div>
+              </div>
+            </motion.div>
 
 
+            <motion.div
+              className="absolute z-10 top-5 right-5"
+              {...fadeAnimation}>
+
+
+              <CustomButton
+                type="filled"
+                title="Go Back"
+                handleClick={() =>
+                  state.intro = true
+                }
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
 
             </motion.div>
+
+
+                    <motion.div 
+                    className='filtertabs-container'
+                    {...slideAnimation('up')}
+                    >
+
+{
+                    FilterTabs.map((tab) => {
+                      <Tab
+                        key={tab.name}
+                        tab={tab}
+                        isFilterTab
+                        isActiveTab=""
+                        handleClick={()=>{}}
+
+                        handleclick={() => { }}
+                      />
+                    })
+                  }
+                    </motion.div>
+
+                    
+
+
           </>
         )
       }
